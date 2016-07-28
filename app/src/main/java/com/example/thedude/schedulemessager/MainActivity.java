7e6a370831da7f2ebc9a2b82d7f963be94f53f85
Mark Yep <mark_yep@hotmail.com>
2016-07-28 17:33:43 +1200

    comments

--- app/src/main/java/com/example/thedude/schedulemessager/MainActivity.java
+++ app/src/main/java/com/example/thedude/schedulemessager/MainActivity.java
@@ -4,8 +4,12 @@
 //Currently we have both timedelay input fields added to the layout.
 //In the future we can add something to select the contact we want to message
 
+//YOLOSWAGTROLL
 package com.example.thedude.schedulemessager;
 
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
